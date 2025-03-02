a39f08484c5b7e82e84321c7bec4e53dde1d63f1
doug <dm@dougmellon.com>
2025-03-01 23:45:05 -0700

    Removing debugging

--- src/prophet_model.rs
+++ src/prophet_model.rs
@@ -8,11 +8,5 @@ pub fn prophet_model (timestamps: Vec<i64>, values: Vec<f64>) -> Predictions {
     
     prophet.fit(data.expect("Failed to gather data to fit"), Default::default()).expect("Failed to fit model");
     
-    let predictions = prophet.predict(None).expect("Failed to make predictions");
-    
-    println!("Predictions: {:?}", predictions.yhat.point);
-    println!("Lower bounds: {:?}", predictions.yhat.lower.as_ref().unwrap());
-    println!("Upper bounds: {:?}", predictions.yhat.upper.as_ref().unwrap());
-    
-    predictions
+    prophet.predict(None).expect("Failed to make predictions")
 }
